f3c3b5b59fc2ff6ebf7b4bb280ea1bea9e7f13b4
DG02 <72976359+Dibyajyoti2002@users.noreply.github.com>
2022-08-26 19:43:02 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const bodyParser=require("body-parser")
+const app=express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post("/",function(req,res){
+    var num1=Number(parseInt(req.body.num1)) 
+    var num2=Number(parseInt(req.body.num2))
+    var result=num1+num2
+    res.send("The result of the calculation is "+result)
+    console.log("Hello")
+
+})
+
+app.listen("3000",function(){
+    console.log("Server listening on port 3000")
+})
