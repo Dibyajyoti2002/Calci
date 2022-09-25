1e8d6be8337675c4e3a3357e45ef7a7966a782c2
DG02 <72976359+Dibyajyoti2002@users.noreply.github.com>
2022-09-25 09:04:39 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -17,6 +17,6 @@ app.post("/",function(req,res){
 
 })
 
-app.listen(process.env.PORT,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server listening on port 3000")
 })
